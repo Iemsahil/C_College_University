bc19d9ca3bddb32a98d46b12c83d4cb6ff6e4e44
MD Sahil <ahmadsahil192@gmail.com>
2023-02-28 16:25:28 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Hello, I am learning C with Harry \n");
+
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 45;
+    int b = 69;
+    int c = a + b;
+    printf("the total sum of number is %d \n", c);
+    return 0;
+
+
+}
